76b6b2f10a2c47562fdaa22839ced96edb852f50
RomeoXXIV <romeo.ibraimovski@gmail.com>
2022-08-06 20:55:01 +0200

    Ajout de la javadoc pour la classe SceneReader

--- AppBSP/src/main/java/be/ac/umons/student/utils/SceneReader.java
+++ AppBSP/src/main/java/be/ac/umons/student/utils/SceneReader.java
@@ -9,14 +9,13 @@ import be.ac.umons.student.models.Point;
 import be.ac.umons.student.models.Segment;
 
 /**
- * SceneReader est une classe qui vérifie le format d'un fichier scène, lit une scène à partir d'un fichier.
- *
+ * SceneReader est une classe qui vérifie qu'un fichier soit au format d'un fichier scène et qui lit une scène à partir d'un fichier scène.
  * @author Romeo Ibraimovski
  */
-public class SceneReader { // TODO: à commenter
+public class SceneReader {
 
     private static final Logger LOGGER = Logger.getLogger(SceneReader.class.getName());
-    public static final Color BROWN = new Color(102,51,0);
+    private static final Color BROWN = new Color(102,51,0);
 
     private String fileName;
     private final boolean isSceneFile;
@@ -36,6 +35,11 @@ public class SceneReader { // TODO: à commenter
         this(new File(pathFile));
     }
 
+    /**
+     * Vérifie si le fichier est un fichier scène.
+     * @param file Fichier à vérifier.
+     * @return True si le fichier est un fichier scène, false sinon.
+     */
     public static boolean isSceneFile(File file) {
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
             String firstLine = bufferedReader.readLine();
@@ -58,11 +62,21 @@ public class SceneReader { // TODO: à commenter
         }
     }
 
+    /**
+     * Vérifie si la ligne est une première ligne d'un fichier scène.
+     * @param firstLine Ligne à vérifier.
+     * @return True si la ligne est une première ligne d'un fichier scène, false sinon.
+     */
     public static boolean isSceneFileFirstLine(String firstLine) {
         String[] split = firstLine.split(" ");
         return split.length == 4 && split[0].equals(">") && isInteger(split[1]) && isInteger(split[2]) && isInteger(split[3]);
     }
 
+    /**
+     * Vérifie si la chaîne de caractères est un entier.
+     * @param str Chaîne de caractères à vérifier.
+     * @return True si la chaîne de caractères est un entier, false sinon.
+     */
     public static boolean isInteger(String str) {
         if (str == null) return false;
         int length = str.length();
@@ -79,11 +93,21 @@ public class SceneReader { // TODO: à commenter
         return true;
     }
 
+    /**
+     * Vérifie si la ligne est une ligne d'un fichier scène.
+     * @param line Ligne à vérifier.
+     * @return True si la ligne est une ligne d'un fichier scène, false sinon.
+     */
     public static boolean isSceneFileLine(String line) {
         String[] split = line.split(" ");
         return split.length == 5 && isSceneFileDouble(split[0]) && isSceneFileDouble(split[1]) && isSceneFileDouble(split[2]) && isSceneFileDouble(split[3]) && isSceneFileColor(split[4]);
     }
 
+    /**
+     * Vérifie si la chaîne de caractères est un double au format "(-)##0.000000".
+     * @param str Chaîne de caractères à vérifier.
+     * @return True si la chaîne de caractères est un double, false sinon.
+     */
     public static boolean isSceneFileDouble(String str) {
         if (str == null) return false;
         int length = str.length();
@@ -100,10 +124,19 @@ public class SceneReader { // TODO: à commenter
         return true;
     }
 
+    /**
+     * Vérifie si la chaîne de caractères est une couleur disponible.
+     * @param str Chaîne de caractères à vérifier.
+     * @return True si la chaîne de caractères est une couleur disponible, false sinon.
+     */
     public static boolean isSceneFileColor(String str) {
         return str.equals("Rouge") || str.equals("Bleu") || str.equals("Gris") || str.equals("Vert") || str.equals("Orange") || str.equals("Violet") || str.equals("Jaune") || str.equals("Rose") || str.equals("Marron") || str.equals("Noir");
     }
 
+    /**
+     * Lit une scène à partir d'un fichier scène.
+     * @param sceneFile Fichier à lire.
+     */
     public void readSceneFile(File sceneFile) {
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(sceneFile))) {
             String firstLine = bufferedReader.readLine();
@@ -123,6 +156,11 @@ public class SceneReader { // TODO: à commenter
         }
     }
 
+    /**
+     * Convertit une chaîne de caractères en segment.
+     * @param str Chaîne de caractères à convertir.
+     * @return Segment correspondant à la chaîne de caractères.
+     */
     public Segment stringToSegment(String str) {
         String[] split = str.split(" ");
         Point p1 = new Point(Double.parseDouble(split[0]), Double.parseDouble(split[1]));
@@ -131,6 +169,11 @@ public class SceneReader { // TODO: à commenter
         return new Segment(p1, p2, color);
     }
 
+    /**
+     * Convertit une chaîne de caractères en couleur.
+     * @param color Chaîne de caractères à convertir.
+     * @return Couleur correspondant à la chaîne de caractères.
+     */
     public Color stringToColor(String color){
         switch (color) {
             case "Rouge":
